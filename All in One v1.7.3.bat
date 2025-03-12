a7aee4763b181ef995310877cff629af2431e6f5
Vanny <147380142+VannyLD@users.noreply.github.com>
2025-03-13 03:48:13 +0700

    Update All in One v1.7.3.bat

--- All in One v1.7.3.bat
+++ All in One v1.7.3.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Downloading latest version...
-gdown "https://drive.google.com/uc?id=1QNx3RTnkuDn0D22zgGVM5yiGdOQLq8sl" -O "All_in_One_v1.7.4.exe"
+curl -L -o "All_in_One_v1.7.4.exe" "https://drive.google.com/uc?export=download&id=1QNx3RTnkuDn0D22zgGVM5yiGdOQLq8sl"
 echo Download complete!
 pause
