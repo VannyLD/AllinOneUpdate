9b1220636ff01852f16ffbafecdb6b6f3d1227df
Vanny <147380142+VannyLD@users.noreply.github.com>
2025-03-13 03:17:21 +0700

    Create All in One v1.7.3.bat

---
+++ All in One v1.7.3.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Downloading file from Google Drive...
+curl -L -o downloaded_file.ext "https://drive.google.com/uc?export=download&id=1QNx3RTnkuDn0D22zgGVM5yiGdOQLq8sl"
+echo Download complete!
+pause
