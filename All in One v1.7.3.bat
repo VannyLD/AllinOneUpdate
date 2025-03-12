bfd64e5ea96a6b495f07b02ef04789a73d58a54b
Vanny <147380142+VannyLD@users.noreply.github.com>
2025-03-13 03:42:14 +0700

    Update All in One v1.7.3.bat

--- All in One v1.7.3.bat
+++ All in One v1.7.3.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Downloading latest version...
-PowerShell -Command "& {Invoke-WebRequest -Uri 'https://drive.usercontent.google.com/download?id=1QNx3RTnkuDn0D22zgGVM5yiGdOQLq8sl&export=download&authuser=0&confirm=t&uuid=510a94a4-177d-44a7-b4da-2d4924c17fb0&at=AEz70l4GcPmNAPm0oEZUton4kMv8:1741811441686' -OutFile 'All_in_One v1.7.4.exe'}"
+gdown "https://drive.google.com/uc?id=1QNx3RTnkuDn0D22zgGVM5yiGdOQLq8sl" -O "All_in_One_v1.7.4.exe"
 echo Download complete!
 pause
